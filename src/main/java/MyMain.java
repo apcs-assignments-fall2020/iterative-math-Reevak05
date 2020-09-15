951552668ed247321538bf23bd0439c6e0543cfe
Reevak05 <kaveergera@gmail.com>
2020-09-15 05:33:23 +0000

    add code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,29 +4,47 @@ public class MyMain {
     
     // Calculates the square root iteratively, using the Babylonian method
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double n = 2;
+        double m = 1;
+        while (Math.abs(m-n)>=0.0001) {
+            n=m;
+            m = (n+(x/n))/2;
+           
+            System.out.println("n="+n);
+        }
+        return n;
     }
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        if (x==0) return 1;
+        
+        int a = 1;
+        for (int i = 1; i <= x; i++) a = a*i;
+        return a;
+        
     }
 
     // Calculates the value for the math constant e iteratively
     // goes until it calculates an answer less than 0.00000000001 
     // different from the value Math.E
     public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double e = 0;
+        int j = 0;
+        while (Math.E-e>=0.00001) {
+            e = e + (1/factorial(j));
+            j++;
+        }
+        return e;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // YOUR CODE HERE
+        System.out.println("Please enter a positive number: ");
+        double entry = scan.nextDouble();
+        System.out.println("The sqaure root of your number is approximately" + babylonian(entry) + ".");
+        System.out.println("The value of e is approximately " + calculateE() + ".");
 
 
         
